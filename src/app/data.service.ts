6aa692a7f3850a46bf8ba2ebf9b39af366b0c687
Krishna Kant <tripathikrishnakanttripathi@gmail.com>
2023-04-06 01:25:22 +0530

    need to add ngfor with flex

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import data from '../assets/data.json';
+import jsonData from 'src/assets/data.json';
+import { BlogData } from './models/interfaces/blog.interface';
 
 @Injectable({
   providedIn: 'root'
 })  
 export class DataService {
 
-  private data: {[key: string]: string} = {};
-
-  constructor() { }
-
-  public getData(key: string): any {
-    return this.data[key];
+  data: BlogData[] = [];
+  constructor(){}
+  
+  getData(){
+    this.data = jsonData.map((item:BlogData)=>Object.assign({},item)) as BlogData[];
+    return this.data;
   }
 
 }
